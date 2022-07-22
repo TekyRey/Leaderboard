8b55868959e59ecc4769244e43a3ce9ff4221aba
TekyRey <mwakabayah@gmail.com>
2022-07-22 08:37:35 +0300

    Add create new game functionality

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import "./style.css";
+import './style.css';
 
-const refresh = document.querySelector(".btn-refresh");
-const form = document.getElementById("form");
-const listScore = document.getElementById("score_list");
+const refresh = document.querySelector('.btn-refresh');
+const form = document.getElementById('form');
+const listScore = document.getElementById('score_list');
 
-const gameId = "Ni1OxKmEJWvyYe0xcLQq";
+const gameId = 'Ni1OxKmEJWvyYe0xcLQq';
 
 const submitScore = async (userName, userScore) => {
   const response = await fetch(
     `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`,
     {
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify({
         user: userName,
         score: userScore,
       }),
       headers: {
-        "Content-Type": "application/json",
-        charset: "UTF-8",
+        'Content-Type': 'application/json',
+        charset: 'UTF-8',
       },
     }
   );
@@ -33,22 +33,22 @@ const fetchData = async () => {
   const resultArray = getResult.result;
   const values = resultArray
     .map(
-      (result) => `<li class="display-table-list">
+      (result) => `<li class='display-table-list'>
                       <p>${result.user}: ${result.score}</p>
                   </li>`
     )
-    .join("");
+    .join('');
   listScore.innerHTML = values;
 };
 
-form.addEventListener("submit", async (e) => {
+form.addEventListener('submit', async (e) => {
   e.preventDefault();
   await submitScore(form.name.value, form.score.value);
-  form.name.value = "";
-  form.score.value = "";
+  form.name.value = '';
+  form.score.value = '';
 });
 
-refresh.addEventListener("click", async () => {
+refresh.addEventListener('click', async () => {
   fetchData();
 });
 
